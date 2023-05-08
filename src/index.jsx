dfb2a404839f19788620245d34d994fedfe7dfbe
Antonio Alan <tonio.alan07@gmail.com>
2023-05-08 10:56:44 -0300

    Feed header e pasta de cores e reset

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './styles/generic/reset.css'
+import './styles/settings/colors.css'
 import App from './App.jsx'
 import './index.css'
 
--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,15 +1,22 @@
+import styles from './styles.module.css';
 import { Link } from "react-router-dom";
 
 const Feed = () => {
-    return(
+    return (
         <>
-            <h1>
-                Page Feed
-            </h1>
+            <div className={styles.container_header}>
+                <div className={styles.logo}>
+                    <h1>
+                        CRUder
+                    </h1>
+                </div>
 
-            <Link to="/post">
-                <button>Add New Post</button>
-            </Link>
+                <div className={styles.btn_new}>
+                    <Link to="/post">
+                        <button>Add New Post</button>
+                    </Link>
+                </div>
+            </div>
         </>
     )
 }
