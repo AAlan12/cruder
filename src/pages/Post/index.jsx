653a15e7540c0c34c64e4cc07fea5de17b6a0cc8
Antonio Alan <tonio.alan07@gmail.com>
2023-05-08 16:20:26 -0300

    ajustes no back-button do Post

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -1,15 +1,15 @@
+import styles from './styles.module.css';
+import BackButton from '../../assets/back-button.svg'
 import { Link } from "react-router-dom";
 
 const Post = () => {
-    return(
+    return (
         <>
-            <h1>
-                Page Post
-            </h1>
-
-            <Link to="/">
-                <button>Back</button>
-            </Link>
+            <div className={styles.container_header}>
+                <Link to="/">
+                    <img src={BackButton} alt="Botão de voltar" />
+                </Link>
+            </div>
         </>
     )
 }
