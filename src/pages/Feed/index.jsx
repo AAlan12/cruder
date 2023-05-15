2a7c2008c6e6c8660270d6805577f6a24f589e75
Antonio Alan <tonio.alan07@gmail.com>
2023-05-15 10:17:45 -0300

    add useEffect no Feed Page

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,8 +1,35 @@
 import HeaderMain from '../../components/HeaderMain';
 import styles from './styles.module.css';
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const Feed = () => {
+
+    const [ posts, setPosts ] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/projects")
+        .then((response) => {
+            setPosts(response.data)
+        })
+
+        .catch(() => {
+            console.log("Deu errrado")
+        })
+
+    }, [])
+
+    function deletePost(id) {
+
+        axios.delete(`http://localhost:5000/projects/${id}`)
+
+        setPosts(posts.filter(post => post._id !== id ))
+
+    }
+
+
     return (
         <>
            <HeaderMain />
