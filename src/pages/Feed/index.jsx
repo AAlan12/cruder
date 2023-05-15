2192a4e1c524a46c289ac35c0247900bdee2d846
Antonio Alan <tonio.alan07@gmail.com>
2023-05-15 10:38:25 -0300

    add main div no Feed Page

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -3,21 +3,22 @@ import styles from './styles.module.css';
 import { Link } from "react-router-dom";
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import More from '../../assets/more.svg'
 
 
 const Feed = () => {
 
-    const [ posts, setPosts ] = useState([])
+    const [posts, setPosts] = useState([])
 
     useEffect(() => {
         axios.get("http://localhost:5000/projects")
-        .then((response) => {
-            setPosts(response.data)
-        })
+            .then((response) => {
+                setPosts(response.data)
+            })
 
-        .catch(() => {
-            console.log("Deu errrado")
-        })
+            .catch(() => {
+                console.log("Deu errrado")
+            })
 
     }, [])
 
@@ -25,18 +26,57 @@ const Feed = () => {
 
         axios.delete(`http://localhost:5000/projects/${id}`)
 
-        setPosts(posts.filter(post => post._id !== id ))
+        setPosts(posts.filter(post => post._id !== id))
 
     }
 
 
     return (
         <>
-           <HeaderMain />
+            <HeaderMain />
 
             <div className={styles.container_main}>
-                <div className={styles.card}>
-                    
+                <div className={styles.cards}>
+                    {posts.map((post, key) => {
+
+                        return (
+
+                            <div className={styles.card} key={key} >
+
+                                <header>
+                                    <h2>{post.title}</h2>
+                                    <img src={More} />
+                                </header>
+
+                                <div className={styles.line}></div>
+
+                                <p>{post.description}</p>
+
+                                <div className={styles.btns} >
+
+                                    <div className={styles.btn_edit} >
+                                        <Link to={{ pathname: `/edit/${post._id}` }} >
+                                            <button>Edit</button>
+                                        </Link>
+                                    </div>
+
+                                    <div className={styles.btn_readmore} >
+                                        <Link to={{ pathname: `/lermais/${post._id}` }} >
+                                            <button>Ler mais</button>
+                                        </Link>
+                                    </div>
+
+                                    <div className={styles.btn_delete} >
+                                        <button onClick={() => deletePost(post._id)} >delete</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        )
+
+                    })}
+
+
                 </div>
 
             </div>
