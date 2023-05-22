4749f0d53f8ac151ab26c5d69cd65988cb64eade
Antonio Alan <tonio.alan07@gmail.com>
2023-05-22 14:01:27 -0300

    GET por parâmetros

--- src/pages/OnePost/index.jsx
+++ src/pages/OnePost/index.jsx
@@ -1,14 +1,24 @@
 import "./styles.css";
+import { useEffect, useState } from "react";
+import { api } from '../../lib/axios';
+import { useParams } from "react-router-dom";
 
 export function OnePost() {
+
+  const [post, setPost] = useState({})
+  const { id } = useParams()
+
+  useEffect(() => {
+    api.get(`/posts/${id}`)
+    .then(response => setPost(response.data))
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <article className="onePostContainer">
-      <h2>Olá esse é o projeto</h2>
+      <h2>{post.title}</h2>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-        inventore officia laborum, similique at ab aut eveniet corporis
-        dignissimos vitae aperiam tempore repellendus ad vero nulla voluptatem
-        molestiae nostrum tenetur!
+        {post.content}
       </p>
     </article>
   );
